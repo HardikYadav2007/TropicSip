a6259aa45ef0e40f875e4edcb5847b1968727c4d
Hardik <hmhardik2007@gmail.com>
2026-08-19 12:08:25 +0530

    initial stage

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,21 @@
+
+function Navbar() {
+    const links = ["Home", "Products", "About", "Contact"];
+
+    return (
+        <nav className="NavBar">
+            <h1 className="siteLogo">TropicSip</h1>
+            <ul style={{ display: "flex", gap: "1rem", alignItems: "center", listStyle: "none" }}>
+                {links.map((link, index) => (
+                    <li key={index}>{link}</li>
+                ))}
+            </ul>
+            <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
+                <a href="">Cart</a>
+                <button className="btn">Sign Up</button>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ src/data/initialData.js
@@ -0,0 +1,38 @@
+// Initial Data to show to users
+
+export const initialProducts = [
+    {
+        id: prod_1,
+        name: "name1",
+        flavor: "flavor1",
+        pricePerCarton: 1000,
+        unitsPerCarton: 25,
+        stockCartons: 150,
+        minOrderQty: 3,
+        discription: "#dis",
+        image: "#link",
+        isPopular: false
+    },
+    { id: prod_2, name: "name2", flavor: "flavor2", pricePerCarton: 1000, unitsPerCarton: 25, stockCartons: 150, minOrderQty: 3, discription: "#dis", image: "#link", isPopular: false },
+    { id: prod_3, name: "name3", flavor: "flavor3", pricePerCarton: 1000, unitsPerCarton: 25, stockCartons: 150, minOrderQty: 3, discription: "#dis", image: "#link", isPopular: false },
+    { id: prod_4, name: "name4", flavor: "flavor4", pricePerCarton: 1000, unitsPerCarton: 25, stockCartons: 150, minOrderQty: 3, discription: "#dis", image: "#link", isPopular: false },
+    { id: prod_5, name: "name5", flavor: "flavor5", pricePerCarton: 1000, unitsPerCarton: 25, stockCartons: 150, minOrderQty: 3, discription: "#dis", image: "#link", isPopular: false },
+    { id: prod_6, name: "name6", flavor: "flavor6", pricePerCarton: 1000, unitsPerCarton: 25, stockCartons: 150, minOrderQty: 3, discription: "#dis", image: "#link", isPopular: false },
+    { id: prod_7, name: "name7", flavor: "flavor7", pricePerCarton: 1000, unitsPerCarton: 25, stockCartons: 150, minOrderQty: 3, discription: "#dis", image: "#link", isPopular: false }
+];
+
+// Default System user ans roles --- No Need till phase 2 ---
+// export const initialUsers =[
+//     {
+//         id: "usr_1",
+//         name: "name1",
+//         email: "[EMAIL_ADDRESS]",
+//         password: "[PASSWORD]",
+//         role: "admin",
+//         department: "Stock & inventory Management",
+//         isSystem: false
+//     },
+//     {id:"usr_2",name:"name2",email:"[EMAIL_ADDRESS]",password:"[PASSWORD]",role:"admin",department:"Wholesale Order Processing",isSystem:false},
+//     {id:"usr_3",name:"name3",email:"[EMAIL_ADDRESS]",password:"[PASSWORD]",role:"admin",department:"Direct Sales & Customer Service",isSystem:false},
+//     {id:"usr_4",name:"name4",email:"[EMAIL_ADDRESS]",password:"[PASSWORD]",role:"super_admin",department:"Executive & Admin Controls",isSystem:true},
+// ];
