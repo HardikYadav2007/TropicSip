df18f022aadf604b0f1478db43ac572688cb21c5
Suman Jyotishi <jyotishisuman06@gmail.com>
2026-08-24 06:35:43 +0530

    Seller Form mistakely removed

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -37,6 +37,7 @@ export default function HomePage() {
                         <BottleMagic />
                         <ProductGrid onAddToCart={handleAddToCart} />
                         <About />
+                        <SellerForm/>
                         <Footer />
                     </>
                 )}
